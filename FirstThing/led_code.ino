d1022f588795b56945b3a5951d5d3f97d42e19f2
Zorin Egor <zorin-ep@mail.ru>
2023-10-03 12:49:20 +0800

    code from the lesson

---
+++ FirstThing/led_code.ino
@@ -0,0 +1,20 @@
+int led_pin = 13;
+
+void setup(){
+  pinMode(led_pin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+    if(Serial.available() > 0){
+    char cmd = Serial.read();
+    if (cmd == 'u'){
+      digitalWrite(led_pin, HIGH);
+      Serial.print("on");
+    }
+    if (cmd == 'd'){
+      digitalWrite(led_pin, LOW);
+      Serial.print("led_off");
+    }
+  }
+}
