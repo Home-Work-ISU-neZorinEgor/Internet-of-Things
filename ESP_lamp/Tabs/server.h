303c2ad9bce425a9c735266e157ef3ac8c865a0d
Зорин Егор <92092053+NeZorinEgor@users.noreply.github.com>
2023-10-23 16:59:59 +0800

    Update server.h

--- ESP_lamp/Tabs/server.h
+++ ESP_lamp/Tabs/server.h
@@ -10,6 +10,33 @@ page_code += "</form>";
   server.send(200, "text/html", page_code);
 }
 
+void handle_wifi_setup() {
+  String wifi_setup_page = "<form action=\"/set_wifi\" method=\"POST\">";
+  wifi_setup_page += "SSID: <input type=\"text\" name=\"ssid\"><br>";
+  wifi_setup_page += "Password: <input type=\"password\" name=\"password\"><br>";
+  wifi_setup_page += "<input type=\"submit\" value=\"Set Wi-Fi\" style=\"background-color: #4CAF50; color: white; padding: 10px 20px; border: none; cursor: pointer;\">";
+  wifi_setup_page += "</form>";
+
+  server.send(200, "text/html", wifi_setup_page);
+}
+
+void handle_set_wifi() {
+  if (server.hasArg("ssid") && server.hasArg("password")) {
+    String new_ssid = server.arg("ssid");
+    String new_password = server.arg("password");
+
+    // Подключитесь к новой Wi-Fi сети
+    WiFi.begin(new_ssid.c_str(), new_password.c_str());
+
+    server.send(200, "text/html", "Wi-Fi setup complete. Rebooting...");
+    delay(1000);
+    ESP.restart();
+  } else {
+    server.send(200, "text/html", "Please provide both SSID and password.");
+  }
+}
+
+
 void handle_led(){
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   server.sendHeader("Location", "/");
@@ -22,9 +49,11 @@ void handle_not_found(){
 
 void server_init(){
   server.on("/", HTTP_GET, handle_root);
-  server.on("/LED", HTTP_POST, handle_led); // Исправлено на HTTP_POST
+  server.on("/LED", HTTP_POST, handle_led);
+  server.on("/wifi_setup", HTTP_GET, handle_wifi_setup); // Добавлен новый обработчик
+  server.on("/set_wifi", HTTP_POST, handle_set_wifi); // Добавлен новый обработчик
   server.onNotFound(handle_not_found);
 
   server.begin();
-  Serial.println("server start on port 80"); // Исправлено на Serial.println
+  Serial.println("Server started on port 80");
 }
