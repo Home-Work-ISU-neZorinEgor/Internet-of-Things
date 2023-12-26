9573023921654478232870e1757e73f4b4394c4d
Zorin Egor <zorin-ep@mail.ru>
2023-12-26 20:47:04 +0800

    ref

--- button/button.ino
+++ button/button.ino
@@ -1,8 +1,7 @@
-const int PIN = 2;
-const int BTN_BOUNCE = 50;
+const int PIN = 3;
 const int PRESSED_STATE = LOW;
-
-volatile bool btnPressed = false;
+volatile bool btnPressed = false; //по умолчанию
+const int BTN_BOUNCE = 50; //задержка
 volatile int count = 0;
 
 void setupBtn() {
@@ -30,13 +29,13 @@ void printCount() {
   }
 }
 
-void loop() {
-  delay(1000);
-  printCount();
-}
-
 void setup() {
   Serial.begin(9600);
   setupBtn();
   setupTimer();
+}
+
+void loop() {
+  delay(1000);
+  printCount();
 }
